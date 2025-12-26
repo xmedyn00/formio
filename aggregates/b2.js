77deddcd875c69fa998308463bffd04526ef2dd1
Ivan Medynskyi <medynskyi@seznam,cz>
2025-12-26 13:21:00 +0100

    commit

--- aggregates/b2.js
+++ aggregates/b2.js
@@ -1,5 +1,5 @@
 // B2.js
-const { applySelectBoxesAnoNe } = require('./selectBoxesAnoNe');
+const applySelectBoxesAnoNe = require('./selectBoxesAnoNe');
 
 module.exports = function handleB2(body) {
   if (!body || typeof body !== 'object') return;
