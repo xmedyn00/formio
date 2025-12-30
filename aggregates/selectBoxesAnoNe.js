44ba005ba1251d966310fb0b2108ed5545c8945a
Ivan Medynskyi <medynskyi@seznam,cz>
2025-12-30 10:53:46 +0100

    commit

--- aggregates/selectBoxesAnoNe.js
+++ aggregates/selectBoxesAnoNe.js
@@ -21,10 +21,10 @@ module.exports = function applySelectBoxesAnoNe(
     const checked = Boolean(data[item.value]);
 
     // ✅ ANO
-    body[`${key}_${item.value}_ano`] = checked ? '☒ ANO' : '☐';
+    body[`${key}_${item.value}_ano`] = checked ? '☒' : '☐';
 
     // ❌ NE
-    body[`${key}_${item.value}_ne`] = checked ? '☐' : '☒ NE';
+    body[`${key}_${item.value}_ne`] = checked ? '☐' : '☒';
 	body[`${key}_${item.value}_anone`] = checked ? 'ANO' : 'NE';
   });
 };
