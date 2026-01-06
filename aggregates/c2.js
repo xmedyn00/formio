ebe547f87b0964330d73364e026820056bcf5876
Ivan Medynskyi <medynskyi@seznam,cz>
2026-01-06 18:34:05 +0100

    commit

--- aggregates/c2.js
+++ aggregates/c2.js
@@ -1,20 +1,59 @@
-const applySelectBoxesAnoNe = require('./selectBoxesAnoNe');
-
 module.exports = function handleC2(body) {
-  const key = 'c2_pripravaTepleVody';
-  const valueKey = 'pripravaTepleVody';
 
-  let checkbox = '☐';
+  /**
+   * ☑ SINGLE SELECTBOX → SINGLE CHECKBOX PLACEHOLDER
+   *
+   * body[sourceKey] = { [valueKey]: true | false }
+   * generates:
+   * body[targetKey] = '☒' | '☐'
+   */
+  function applySingleSelectBoxCheckbox(sourceKey, valueKey, targetKey) {
+    let checkbox = '☐';
 
-  if (
-    body[key] &&
-    typeof body[key] === 'object' &&
-    body[key][valueKey] === true
-  ) {
-    checkbox = '☒';
+    if (
+      body[sourceKey] &&
+      typeof body[sourceKey] === 'object' &&
+      body[sourceKey][valueKey] === true
+    ) {
+      checkbox = '☒';
+    }
+
+    body[targetKey] = checkbox;
   }
 
-  // 🔹 ОДИН checkbox placeholder
-  body.c2_pripravaTepleVody_checkbox = checkbox;
+  // =========================
+  // C2 – Příprava teplé vody
+  // =========================
+  applySingleSelectBoxCheckbox(
+    'c2_pripravaTepleVody',
+    'pripravaTepleVody',
+    'c2_pripravaTepleVody_checkbox'
+  );
+
+  // 🔜 další SelectBoxy C2 přidáš stejným způsobem
+  
+  applySingleSelectBoxCheckbox(
+    'c2_pripravaTepleVodyTeplSolarniSoustava',
+    'pripravaTepleVodyTeplSolarniSoustava',
+    'c2_pripravaTepleVodyTeplSolarniSoustava_checkbox'
+  );
+  
+  applySingleSelectBoxCheckbox(
+    'c2_zasobnikTepelnehoCerpadla',
+    'vyrovnavaciZasobnikTepelnehoCerpadla',
+    'c2_zasobnikTepelnehoCerpadla_checkbox'
+  );
+  
+  applySingleSelectBoxCheckbox(
+    'c2_ZasobnikKotleNaTuhaPaliva',
+    'vyrovnavaciZasobnikKotleNaTuhaPaliva',
+    'c2_ZasobnikKotleNaTuhaPaliva_checkbox'
+  );
+  
+  applySingleSelectBoxCheckbox(
+    'c2_sb_jine',
+    'jine',
+    'c2_sb_jine_checkbox'
+  );
 };
 
